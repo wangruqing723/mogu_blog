c1ca8ac71c74d8d9e4cda29107c59bad1051c1b3
wyong <2662446965@qq.com>
2021-01-12 23:19:14 +0800

    修改各个服务的配置文件,部署到虚拟机 (vue_mogu_admin和vue_mogu_web两个前端项目还没有进行install和build)

--- vue_mogu_admin/config/prod.env.js
+++ vue_mogu_admin/config/prod.env.js
@@ -3,19 +3,19 @@ module.exports = {
   NODE_ENV: '"production"',
 
   //生产环境
-  ADMIN_API: '"http://47.115.54.30:8601"',
-  PICTURE_API: '"http://47.115.54.30:8602"',
-  WEB_API: '"http://47.115.54.30:8603"',
-  Search_API: '"http://47.115.54.30:8605"',
-  SPRING_BOOT_ADMIN: '"http://47.115.54.30:8606/wallboard"',
-  SOLR_API: '"http://47.115.54.30:8080/solr"',
-  Zipkin_Admin: '"http://47.115.54.30:9411/zipkin/"',
-  ELASTIC_SEARCH: '"http://47.115.54.30:5601"',
-  EUREKA_API: '"http://47.115.54.30:8761"',
-  RABBIT_MQ_ADMIN: '"http://47.115.54.30:15672"',
-  DRUID_ADMIN: '"http://47.115.54.30:8601/druid/login.html"',
+  ADMIN_API: '"http://192.168.159.135:8601"',
+  PICTURE_API: '"http://192.168.159.135:8602"',
+  WEB_API: '"http://192.168.159.135:8603"',
+  Search_API: '"http://192.168.159.135:8605"',
+  SPRING_BOOT_ADMIN: '"http://192.168.159.135:8606/wallboard"',
+  SOLR_API: '"http://192.168.159.135:8080/solr"',
+  Zipkin_Admin: '"http://192.168.159.135:9411/zipkin/"',
+  ELASTIC_SEARCH: '"http://192.168.159.135:5601"',
+  EUREKA_API: '"http://192.168.159.135:8761"',
+  RABBIT_MQ_ADMIN: '"http://192.168.159.135:15672"',
+  DRUID_ADMIN: '"http://192.168.159.135:8601/druid/login.html"',
   // 有域名
-  BLOG_WEB_URL: '"http://demoweb.moguit.cn"',
+  // BLOG_WEB_URL: '"http://demoweb.moguit.cn"',
   // 没有域名
-  // BLOG_WEB_URL: '"http://47.115.54.30:9527"',
+  BLOG_WEB_URL: '"http://192.168.159.135:9527"',
 }
--- vue_mogu_web/config/prod.env.js
+++ vue_mogu_web/config/prod.env.js
@@ -4,10 +4,10 @@ module.exports = {
   //配置线上环境
 
   // 如果没有域名使用这个
-  //VUE_MOGU_WEB: '"http://47.115.54.30:8602"',
+  VUE_MOGU_WEB: '"http://192.168.159.135:9527"',
   // 如果有域名使用这个
-  VUE_MOGU_WEB: '"http://demoweb.moguit.cn"',
-  PICTURE_API: '"http://47.115.54.30:8602"',
-  WEB_API: '"http://47.115.54.30:8603"',
-  ELASTICSEARCH: '"http://47.115.54.30:8605"',
+  // VUE_MOGU_WEB: '"http://demoweb.moguit.cn"',
+  PICTURE_API: '"http://192.168.159.135:8602"',
+  WEB_API: '"http://192.168.159.135:8603"',
+  ELASTICSEARCH: '"http://192.168.159.135:8605"',
 }
